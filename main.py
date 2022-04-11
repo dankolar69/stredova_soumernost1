f128cb8b66fc14958ee90f3b963d1344a6962f81
dankolar69 <91247767+dankolar69@users.noreply.github.com>
2022-04-11 22:27:24 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+led.plot_brightness(1, 1, 75)
+a = [4, 0]
+b = [0, 4]
+
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+
+def zmena_velikosti():
+    global a, b
+    led.plot(0, 0)
+    
+
+
+
+def on_button_pressed_a():
+    zmena_velikosti()
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function on_forever() {
+led.plotBrightness(1, 1, 75)
+let a = [4, 0]
+let b = [0, 4]
+led.plot(a[0], a[1])
+led.plot(b[0], b[1])
+function zmena_velikosti() {
     
+    led.plot(0, 0)
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    zmena_velikosti()
 })
